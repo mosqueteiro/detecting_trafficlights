f341c4a4ec12e36aaef4ac3fb57bb4c89b61e4eb
mosqueteiro <nat3.th3.gr3at@gmail.com>
2019-04-15 17:41:20 -0600

    add docker compose file and tensorflow-jupyter

--- aws_setup.sh
+++ aws_setup.sh
@@ -6,4 +6,20 @@ curl -sSL https://get.docker.com/ | bash
 # add ubuntu to docker user group
 sudo usermod -aG docker ubuntu
 
+# add docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# clone repo
+git clone https://github.com/mosqueteiro/detecting_trafficlights.git
+
+# download train2017 images
+sudo apt install unzip
+mkdir -p detecting_trafficlights/data/coco/train2017
+# cd detecting_trafficlights/data/coco/train2017
+curl -O http://images.cocodataset.org/zips/train2017.zip
+unzip train2017.zip -d detecting_trafficlights/data/coco/train2017/
+
+
 # exit terminal now
