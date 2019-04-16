81f475e6fda2b0d2de7c76e288fc94973aa54127
mosqueteiro <nat3.th3.gr3at@gmail.com>
2019-04-15 18:43:31 -0600

    update mounting for jupyter_lab; update aws_setup to relog user

--- aws_setup.sh
+++ aws_setup.sh
@@ -5,19 +5,18 @@ curl -sSL https://get.docker.com/ | bash
 
 # add ubuntu to docker user group
 sudo usermod -aG docker ubuntu
+# relog user to apply group add w/o logging out
+exec sudo su -l $USER
 
 # add docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-# clone repo, THIS SHOULD ALREADY BE CLONED TO RUN THIS FILE
-# git clone https://github.com/mosqueteiro/detecting_trafficlights.git
 
 # download train2017 images
 sudo apt install unzip
 mkdir -p data/coco/train2017
-# cd detecting_trafficlights/data/coco/train2017
 curl -O http://images.cocodataset.org/zips/train2017.zip
 unzip train2017.zip -d data/coco/train2017/
 rm train2017.zip
