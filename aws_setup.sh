8ae80c67966db534098d4f8638fb855c47e9784a
mosqueteiro <nat3.th3.gr3at@gmail.com>
2019-04-15 22:16:50 -0600

    ...

--- aws_setup.sh
+++ aws_setup.sh
@@ -5,18 +5,19 @@ curl -sSL https://get.docker.com/ | bash
 
 # add ubuntu to docker user group
 sudo usermod -aG docker ubuntu
+
 # relog user to apply group add w/o logging out
-exec sudo su -l $USER
-
-# add docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-docker-compose up -d
-docker exec detecting_trafficlights_jupyter_flow_1 \
-pip install -r docker/jupyter/requirements.txt
-docker exec detecting_trafficlights_jupyter_flow_1 jupyter notebook list
+# install docker compose
+# install final dependencies
+# run jupyter notebook
+exec sudo su -l $USER \
+&& sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+&& sudo chmod +x /usr/local/bin/docker-compose \
+&& sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose \
+&& cd detecting_trafficlights && docker-compose up -d \
+&& docker exec detecting_trafficlights_jupyter_flow_1 \
+pip install -r docker/jupyter/requirements.txt \
+&& docker exec detecting_trafficlights_jupyter_flow_1 jupyter notebook list
 
 # download train2017 images
 # sudo apt install unzip
