2a4277e5c998dde1deab4a832f148aee335e2b85
mosqueteiro <nat3.th3.gr3at@gmail.com>
2019-04-20 01:01:51 -0600

    use psql -U postgres

--- docker/postgres/at_startup.sh
+++ docker/postgres/at_startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 createdb -U postgres train2017
-psql train2017 < initdb.sql
+psql -U postgres train2017 < initdb.sql
 
 createdb -U postgres val2017
-psql val2017 < initdb.sql
+psql -U postgres val2017 < initdb.sql
