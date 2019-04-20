cc4cac64630e2634d6fbf7130d9cfe0f2b2a11cd
mosqueteiro <nat3.th3.gr3at@gmail.com>
2019-04-20 02:31:18 -0600

    ...

--- src/trafficlight_data.py
+++ src/trafficlight_data.py
@@ -3,22 +3,15 @@ import numpy as np
 import pandas as pd
 
 
-
-dataset = 'train2017'
-store_dir = '/media/mosqueteiro/TOSHIBA EXT/detecting_trafficlights/'
-user = 'mosqueteiro'
-host = '/var/run/postgresql'
-
-
 # train2017.query_database('SELECT * FROM street_annotations')
 # annotations = train2017.df_query
 
 def load_binary_train(**kwargs):
-    dataset = kwargs.get('dataset', dataset)
-    host = kwargs.get('host', host)
-    user = kwargs.get('user', user)
-    data_dir = kwargs.get('data_dir', store_dir)
-    with QueryDatabase(dataset, host, user, data_dir=data_dir) as data:
+    dataset = kwargs.get('dataset', 'train2017')
+    host = kwargs.get('host', 'localhost')
+    user = kwargs.get('user', 'postgres')
+    data_dir = kwargs.get('data_dir', 'data/coco/')
+    with QueryDatabase(dataset, host, user, data_dir) as data:
         data.query_database('SELECT * FROM street_images')
         X = data.df_query.set_index('image_id').sort_index()
         data.query_database('SELECT * FROM street_annotations')
